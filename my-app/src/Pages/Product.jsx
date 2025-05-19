62b221c4241a5fbcff075637218f72c4251473bb
yoyobazzi3 <yoyobazzi3@gmail.com>
2025-05-19 13:34:20 -0700

    feat(src):Product API call

--- my-app/src/Pages/Product.jsx
+++ my-app/src/Pages/Product.jsx
@@ -1,5 +1,28 @@
 import react from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const Products = () =>{
-    
-}
+const Product = () => {
+    const { id } = useParams();
+    const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://cart-api.alexrodriguez.workers.dev/products")
+      .then((res) => {
+        const response = res.data.find((product) => product.id === id);
+        setProduct(response);
+      })
+
+      .catch((err) => console.error(err));
+  }, [id]);
+  if(!product) return <p>Loading...</p>
+
+  return(
+    <div>
+        <h2>{product.name}</h2>
+    </div>
+  )
+};
+export default Product
